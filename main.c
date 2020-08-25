13a5d48f699953f3f69777740a89cf69d483a9df
xiazi21 <zfx5078@psu.edu>
2020-08-25 08:32:33 +0000

    1

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <readline/readline.h>
 
 int main(void) {
-  char *name= readline("Enter Your name");
-  printf("Hello %s!\n", name);
+  char *name= readline("Enter Your name: ");
+  printf("Hello, %s!\n", name);
   return 0;
 }
