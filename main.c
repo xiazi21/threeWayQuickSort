90b1ff27c5fecdae9f6fd8d766b0dcea3c8b9360
xiazi21 <zfx5078@psu.edu>
2020-09-03 06:20:27 +0000

    4

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
-
 #include <stdio.h>
-
 #include <readline/readline.h>
-
-
-
-int main(void) {
-
-  char *name= readline("Enter your name: ");
-
-  printf("Hello, %s!\n", name);
-
-  return 0;
-
+#include <stdlib.h>
+int main(void) 
+{
+  double f;
+ char *temp= readline("Enter temperature in celsius: ");
+ double c = atof(temp);
+ f=(1.8*c)+32;
+ printf("%f° in Celsius is equivalent to %f° Fahrenheit.",c,f);
+return 0;
 }
